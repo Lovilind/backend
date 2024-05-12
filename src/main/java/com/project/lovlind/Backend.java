77d119dd25527055ab234e8267ebc134a998788e
Siglee2247 <lgh941107@gmail.com>
2024-05-12 16:55:27 +0900

    init: init project

---
+++ src/main/java/com/project/lovlind/Backend.java
@@ -0,0 +1,12 @@
+package com.project.lovlind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Backend.class, args);
+  }
+}
---
+++ src/test/java/com/project/lovlind/BackendTests.java
@@ -0,0 +1,11 @@
+package com.project.lovlind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTests {
+
+  @Test
+  void contextLoads() {}
+}
