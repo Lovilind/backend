5af5055730412004055ac8a3a0ca0bfd59405824
Siglee2247 <lgh941107@gmail.com>
2024-05-19 14:49:11 +0900

    [BE-02] participant Cache 설정 추가

---
+++ src/main/java/com/project/lovlind/domain/chat/cache/ParticipantCache.java
@@ -0,0 +1,10 @@
+package com.project.lovlind.domain.chat.cache;
+
+import com.project.lovlind.domain.chat.cache.dto.ParticipantDto;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+// TODO : 추 후 Redis 설정 시 Redis 로 변경할 예정
+public class ParticipantCache {
+  public static final Map<Long, Map<String, ParticipantDto>> participantMap = new ConcurrentHashMap<>();
+}
