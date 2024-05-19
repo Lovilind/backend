67f840e0c0ff4a00f4852ad72099e3e6e228f149
Siglee2247 <lgh941107@gmail.com>
2024-05-19 21:57:46 +0900

    [BE-02] entity 연관관계 및 Enum 오류 수정

--- src/main/java/com/project/lovlind/domain/chat/entity/Message.java
+++ src/main/java/com/project/lovlind/domain/chat/entity/Message.java
@@ -1,16 +1,20 @@
 package com.project.lovlind.domain.chat.entity;
 
+import static jakarta.persistence.EnumType.*;
+
 import com.project.lovlind.domain.auditing.BaseTime;
+import com.project.lovlind.domain.chat.controller.enums.MessageType;
 import com.project.lovlind.domain.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,17 +29,21 @@ public class Message extends BaseTime {
 
   @Lob private String message;
 
-  @ManyToOne
-  @JoinColumn(name = "participant_id")
+  @Enumerated(STRING)
+  private MessageType messageType;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
   Member member;
 
-  @OneToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "chatroom_id")
   Chatroom chatroom;
 
-  public Message(String message, Member member, Chatroom chatroom) {
+  public Message(String message, Member member, Chatroom chatroom, MessageType messageType) {
     this.message = message;
     this.member = member;
     this.chatroom = chatroom;
+    this.messageType = messageType;
   }
 }
