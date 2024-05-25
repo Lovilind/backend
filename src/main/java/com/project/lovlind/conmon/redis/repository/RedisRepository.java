8452e4b884d0ee31ede8b0a46370bac3d19d21d2
Siglee2247 <lgh941107@gmail.com>
2024-05-25 22:46:28 +0900

    [BE-03] 레디스 설정 추가

---
+++ src/main/java/com/project/lovlind/conmon/redis/repository/RedisRepository.java
@@ -0,0 +1,25 @@
+package com.project.lovlind.conmon.redis.repository;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisRepository {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public void save(String key, String value, int minutes) {
+        redisTemplate.opsForValue().set(key, value, minutes, TimeUnit.MINUTES);
+    }
+
+    public String findByKey(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+}
