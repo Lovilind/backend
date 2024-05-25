337348058cc2672513fed1676fef9da1403c3c21
Siglee2247 <lgh941107@gmail.com>
2024-05-26 00:16:20 +0900

    [BE-03] h2 사용을 위한 설정 추가

--- src/main/java/com/project/lovlind/conmon/config/SecurityConfig.java
+++ src/main/java/com/project/lovlind/conmon/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -28,6 +29,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.with(jwtFilterDsl, JwtFilterDsl::build);
+    http.headers(headerConfig -> headerConfig.frameOptions(FrameOptionsConfig::disable));
     http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
