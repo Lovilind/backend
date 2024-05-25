0bc9a01178cd0ab11a690085671f4cac5bf0de3c
Siglee2247 <lgh941107@gmail.com>
2024-05-25 22:34:39 +0900

    [BE-03] 구글 린트 적용

--- src/main/java/com/project/lovlind/conmon/utils/jwt/JwtProperties.java
+++ src/main/java/com/project/lovlind/conmon/utils/jwt/JwtProperties.java
@@ -14,14 +14,14 @@ import org.springframework.stereotype.Component;
 @PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
 public class JwtProperties {
 
-    private String accessHeader;
-    private String refreshHeader;
-    private String prefix;
-    private String secretKey;
-    private Integer accessTokenValidityInSeconds;
-    private Integer refreshTokenValidityInSeconds;
+  private String accessHeader;
+  private String refreshHeader;
+  private String prefix;
+  private String secretKey;
+  private Integer accessTokenValidityInSeconds;
+  private Integer refreshTokenValidityInSeconds;
 
-    public int getPrefixLength() {
-        return prefix.length();
-    }
+  public int getPrefixLength() {
+    return prefix.length();
+  }
 }
--- src/main/java/com/project/lovlind/conmon/utils/jwt/JwtProvider.java
+++ src/main/java/com/project/lovlind/conmon/utils/jwt/JwtProvider.java
@@ -14,58 +14,58 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
-    private final JwtProperties jwtProperties;
+  private final JwtProperties jwtProperties;
 
-    public String generateAccessToken(String subject, Long id, String authorities) {
-        Date expiration = getAccessExpiration();
-        return Jwts.builder()
-            .subject(subject)
-            .expiration(expiration)
-            .claims(createClaims(id, authorities))
-            .signWith(getEncodedKey())
-            .compact();
-    }
+  public String generateAccessToken(String subject, Long id, String authorities) {
+    Date expiration = getAccessExpiration();
+    return Jwts.builder()
+        .subject(subject)
+        .expiration(expiration)
+        .claims(createClaims(id, authorities))
+        .signWith(getEncodedKey())
+        .compact();
+  }
 
-    public String generateRefreshToken(String subject) {
-        return Jwts.builder()
-            .subject(subject)
-            .expiration(getRefreshExpiration())
-            .signWith(getEncodedKey())
-            .compact();
-    }
+  public String generateRefreshToken(String subject) {
+    return Jwts.builder()
+        .subject(subject)
+        .expiration(getRefreshExpiration())
+        .signWith(getEncodedKey())
+        .compact();
+  }
 
-    public Claims getClaims(String token) {
-        return Jwts.parser()
-            .verifyWith(getEncodedKey())
-            .clockSkewSeconds(60)
-            .build()
-            .parseSignedClaims(token)
-            .getPayload();
-    }
+  public Claims getClaims(String token) {
+    return Jwts.parser()
+        .verifyWith(getEncodedKey())
+        .clockSkewSeconds(60)
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
 
-    public Integer getRefreshTokenValidityInSeconds() {
-        return jwtProperties.getRefreshTokenValidityInSeconds();
-    }
+  public Integer getRefreshTokenValidityInSeconds() {
+    return jwtProperties.getRefreshTokenValidityInSeconds();
+  }
 
-    private Date getAccessExpiration() {
-        return addExpirationData(jwtProperties.getAccessTokenValidityInSeconds()).getTime();
-    }
+  private Date getAccessExpiration() {
+    return addExpirationData(jwtProperties.getAccessTokenValidityInSeconds()).getTime();
+  }
 
-    private Date getRefreshExpiration() {
-        return addExpirationData(jwtProperties.getRefreshTokenValidityInSeconds()).getTime();
-    }
+  private Date getRefreshExpiration() {
+    return addExpirationData(jwtProperties.getRefreshTokenValidityInSeconds()).getTime();
+  }
 
-    private Calendar addExpirationData(int expirationMinutes) {
-        Calendar now = Calendar.getInstance();
-        now.add(Calendar.MINUTE, expirationMinutes);
-        return now;
-    }
+  private Calendar addExpirationData(int expirationMinutes) {
+    Calendar now = Calendar.getInstance();
+    now.add(Calendar.MINUTE, expirationMinutes);
+    return now;
+  }
 
-    private SecretKey getEncodedKey() {
-        return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
-    }
+  private SecretKey getEncodedKey() {
+    return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
+  }
 
-    private Map<String, Object> createClaims(Long id, String authorities) {
-        return Map.of("id", id, "authorities", authorities);
-    }
+  private Map<String, Object> createClaims(Long id, String authorities) {
+    return Map.of("id", id, "authorities", authorities);
+  }
 }
