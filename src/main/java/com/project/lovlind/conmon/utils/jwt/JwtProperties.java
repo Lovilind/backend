28ec5de04fe96d2fafe7e3fc39ca3982aa6716e9
Siglee2247 <lgh941107@gmail.com>
2024-05-25 22:26:31 +0900

    [BE-03] jwt utils 클래스 구현 - jwt 변환을 위한 utils 클래스 구현

---
+++ src/main/java/com/project/lovlind/conmon/utils/jwt/JwtProperties.java
@@ -0,0 +1,27 @@
+package com.project.lovlind.conmon.utils.jwt;
+
+import com.project.lovlind.conmon.config.property.YamlPropertySourceFactory;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("jwt")
+@PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
+public class JwtProperties {
+
+    private String accessHeader;
+    private String refreshHeader;
+    private String prefix;
+    private String secretKey;
+    private Integer accessTokenValidityInSeconds;
+    private Integer refreshTokenValidityInSeconds;
+
+    public int getPrefixLength() {
+        return prefix.length();
+    }
+}
---
+++ src/main/java/com/project/lovlind/conmon/utils/jwt/JwtProvider.java
@@ -0,0 +1,71 @@
+package com.project.lovlind.conmon.utils.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateAccessToken(String subject, Long id, String authorities) {
+        Date expiration = getAccessExpiration();
+        return Jwts.builder()
+            .subject(subject)
+            .expiration(expiration)
+            .claims(createClaims(id, authorities))
+            .signWith(getEncodedKey())
+            .compact();
+    }
+
+    public String generateRefreshToken(String subject) {
+        return Jwts.builder()
+            .subject(subject)
+            .expiration(getRefreshExpiration())
+            .signWith(getEncodedKey())
+            .compact();
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+            .verifyWith(getEncodedKey())
+            .clockSkewSeconds(60)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    public Integer getRefreshTokenValidityInSeconds() {
+        return jwtProperties.getRefreshTokenValidityInSeconds();
+    }
+
+    private Date getAccessExpiration() {
+        return addExpirationData(jwtProperties.getAccessTokenValidityInSeconds()).getTime();
+    }
+
+    private Date getRefreshExpiration() {
+        return addExpirationData(jwtProperties.getRefreshTokenValidityInSeconds()).getTime();
+    }
+
+    private Calendar addExpirationData(int expirationMinutes) {
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.MINUTE, expirationMinutes);
+        return now;
+    }
+
+    private SecretKey getEncodedKey() {
+        return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Map<String, Object> createClaims(Long id, String authorities) {
+        return Map.of("id", id, "authorities", authorities);
+    }
+}
