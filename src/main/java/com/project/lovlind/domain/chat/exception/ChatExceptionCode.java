76ec78aea97dd78933898f9006def1b6aad16da7
jangsen <dh7895210@naver.com>
2024-06-03 17:21:00 +0900

    [BE-06] ChatExceptionCode 수정

--- src/main/java/com/project/lovlind/domain/chat/exception/ChatExceptionCode.java
+++ src/main/java/com/project/lovlind/domain/chat/exception/ChatExceptionCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ChatExceptionCode implements ExceptionCode {
-  NOT_FOUND_CHATROOM(HttpStatus.FOUND, "채팅방을 찾을 수 없 습니다.");
+  NOT_FOUND_CHATROOM(HttpStatus.FOUND, "채팅방을 찾을 수 없 습니다."),
+  NOT_FOUND_PARTICIPANT(HttpStatus.NOT_FOUND, "참가자를 찾을 수 없습니다."),
+  NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String message;
